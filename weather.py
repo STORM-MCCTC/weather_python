65b76b744a09c15d8a98d998991e2c2c7ba4bf50
STORM <stormvtube@gmail.com>
2024-10-04 10:21:10 -0400

    ...

--- weather.py
+++ weather.py
@@ -23,12 +23,12 @@ if response.status_code == 200:
     
     current_temp = soup.find("span", class_="wu-value wu-value-to").text
     high_temp = soup.find("span", class_="hi").text
-    low_temp = soup.find("span", class_="low-temp-text").text
-    weather_canditions = soup.find("div", class_="conditions-icon").text
+    low_temp = soup.find("span", class_="lo").text
+    weather_canditions = soup.find("div", class_="condition-icon small-6 medium-12 columns").text
 
     print(f"Current Temperature in {City.title()}, {State.title()}: {current_temp}°F")
-    print(f"High Temp {high_temp}°F")
-    print(f"Low Temp {low_temp}°F")
+    print(f"High Temp {high_temp}F")
+    print(f"Low Temp {low_temp}F")
     print(f"weather Conditions: {weather_canditions}")
 else:
     print("Weather data fetch failed. status code:", response.status_code)
